76132a7736c49ff1c8c0a20424fdc979f4f51261
Marcosbartoszyk <130564602+Marcosbartoszyk@users.noreply.github.com>
2024-05-24 19:47:47 -0300

    Upload primeira versão
    Index.html
    Style.css
    Script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal({reset: true})
+
+sr.reveal('.area-1', {
+    duration: 2000,
+    rotate:{x:0, y:0, z:50}
+})
+
+sr.reveal('.area-2', {
+    duration: 2000,
+    rotate:{x:0, y:50, z:0}
+})
+
+sr.reveal('.area-3', {
+    duration: 2000,
+    rotate:{x:50, y:0, z:0}
+})
